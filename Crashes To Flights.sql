911547d623f22e05ea8155705454d0e2725afedb
Pogodowo <59621040+Pogodowo@users.noreply.github.com>
2024-03-02 09:40:21 +0100

    Add files via upload

---
+++ Crashes To Flights.sql
@@ -0,0 +1,24 @@
+
+--Displays the number of crashes in each year 
+Select [Year of crash], COUNT([Year of crash]) AS [Amount of crashes]
+FROM CrashesUntil2020
+GROUP By [Year of crash]
+ORDER BY 'Year of crash'
+
+
+
+--Displays the number of flights in each year
+SELECT *
+FROM FlightsByYearPivot
+
+--Shows how many crashes occured compared to the total number of flights
+
+WITH CTE_crashes AS(Select [Year of crash], COUNT([Year of crash]) AS [Amount of crashes]
+FROM CrashesUntil2020
+GROUP By [Year of crash])
+
+SELECT C.[Year of crash],F.[Flights] AS 'Flights Total',C.[Amount of crashes],ROUND(C.[Amount of crashes]/F.[Flights]*100,6 ) AS 'Ratio %'
+FROM FlightsByYearPivot F
+INNER JOIN CTE_crashes C
+ON CAST(F.[Year] AS int) = CAST(C.[Year of crash] AS int) 
+ORDER BY  C.[Year of crash]
